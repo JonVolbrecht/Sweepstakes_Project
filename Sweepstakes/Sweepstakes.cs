a746cf87447f2ee7253274b8cfb15438af5e1b1c
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-03-03 14:22:27 -0600

    CreatSweepstakes method added.

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -24,7 +24,12 @@ namespace Sweepstakes
 
         }
 
-        
+        public void CreateSweepstakes()
+        {
+
+            sweepstakesName = UserInterface.NameSweepstakes();
+
+        }
 
 
 

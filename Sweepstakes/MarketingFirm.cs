191e17acc16137b1e78414e04e30674dc82f5650
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-03-04 13:22:36 -0600

    MerketingFirmActions method added. ISweepstakesManager implemented in MarketingFirm.

--- Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/MarketingFirm.cs
@@ -8,5 +8,31 @@ namespace Sweepstakes
 {
     class MarketingFirm
     {
+        ISweepstakesManager sweepstakesManager;
+        public void MarketingFirmActions()
+        {
+            while (true)
+            {
+                switch (UserInterface.GetMarketingFirmAction())
+                {
+                    case "1":
+                        FactoryPattern.ChooseSweepstakesManager();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        MarketingFirmActions();
+                        break;
+                }
+            }
+        }
     }
 }

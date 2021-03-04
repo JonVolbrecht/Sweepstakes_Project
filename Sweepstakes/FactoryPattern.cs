f7b44519469936f839230bed776a6fa1066f2db1
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-03-04 13:10:35 -0600

    FactoryPattern method ChooseSweepstakesManager added

--- Sweepstakes/FactoryPattern.cs
+++ Sweepstakes/FactoryPattern.cs
@@ -8,6 +8,20 @@ namespace Sweepstakes
 {
     class FactoryPattern
     {
-        
+        public static ISweepstakesManager ChooseSweepstakesManager()
+        {
+            string UserInterface.UserInput("Choose preferred sweepstakes manager:\nFor stack style - Press '1'\nFor queue style - Press '2'");
+            switch (userInput)
+            {
+                case "1":
+                    return new SweepstakesStackManager();
+
+                case "2":
+                    return new SweepstakesQueueManager();
+   
+                default:
+                    throw new Exception("Enter Valid Input");
+            }
+        }
     }
 }

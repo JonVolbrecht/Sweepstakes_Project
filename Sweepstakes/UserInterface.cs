273634d1f3288f3c513ac138fb9818e69abb3cd3
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-03-04 13:39:31 -0600

    UserInterface class finally completed in its entirety.

--- Sweepstakes/UserInterface.cs
+++ Sweepstakes/UserInterface.cs
@@ -8,5 +8,38 @@ namespace Sweepstakes
 {
     class UserInterface
     {
+        public static string UserInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        public static string SetFirstName()
+        {
+            string userInput = UserInput("Enter first name");
+            return userInput;
+        }
+        public static string SetLastName()
+        {
+            string userInput = UserInput("Enter last name");
+            return userInput;
+        }
+        public static string SetEmailAddress()
+        {
+            string userInput = UserInput("Enter email address");
+            return userInput;
+        }
+
+        public static string NameSweepstakes()
+        {
+            string userInput = UserInput("Enter sweepstakes name");
+            return userInput;
+        }
+
+        public static string GetMarketingFirmAction()
+        {
+            string userInput = UserInput("Choose acion:\nTo choose sweepstakes manager type - Press '1'\nTo create a contestant - Press '2'\nTo create a sweepstakes - Press '3'\nTo add a contestant to a sweepstakes - Press '4'\nTo exit application - Press '5'");
+            return userInput;
+        }
     }
 }
